4b837279d5ea0792c3a52e0f18801a9156117a55
giammarcoditomassi <giammarco.ditomassi@gmail.com>
2021-07-20 14:18:21 +0200

    aggiunte eccezioni, problema su stat

--- server/src/main/java/it/corso/rubjdbc/dao/RubricaDAO.java
+++ server/src/main/java/it/corso/rubjdbc/dao/RubricaDAO.java
@@ -10,57 +10,65 @@ import java.util.List;
 import it.corso.rubjdbc.model.Contatto;
 
 public class RubricaDAO {
-	
+
 	private static RubricaDAO rubDao;
-	
-	private RubricaDAO () {	
+
+	private RubricaDAO() {
 	}
-	
-	public static RubricaDAO getDao () {
+
+	public static RubricaDAO getDao() {
 		if (rubDao == null) {
 			rubDao = new RubricaDAO();
-		} return rubDao;
+		}
+		return rubDao;
 	}
-	
-	public Contatto salva (Contatto c) {
-		return c;	
+
+	public Contatto salva(Contatto c) {
+		return c;
 	}
-	
-	public void rimuovi (Contatto c) {
+
+	public void rimuovi(Contatto c) {
 	}
-	
-	public List<Contatto> trovaTutti(){
-		Connection con= null;
+
+	public List<Contatto> trovaTutti() {
+		Connection con = null;
 		Statement stmt = null;
-		List<Contatto> lista = new ArrayList <>();
+		List<Contatto> lista = new ArrayList<>();
 		try {
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/rubricarand", "root", "");
-			
+
 			Statement stat = con.createStatement();
 			String sql = "select * from contatto";
-			
+
 			ResultSet res = stmt.executeQuery(sql);
-			
+
 			while (res.next()) {
 				long id = res.getLong("id");
 				String fn = res.getString("nome");
 				String ln = res.getString("cognome");
 				String ph = res.getString("telefono");
-				
-				Contatto cx = new Contatto(id, fn,ln, ph);
+
+				Contatto cx = new Contatto(id, fn, ln, ph);
 				cx.setId(id);
 				lista.add(cx);
-				System.out.println("id = " + id + "; Nome = " + fn +"; Cognome= "+ ln + "; Telefono = " + ph);
-				
-			}
-		
-		
-		return null;
-		
-	}
-	
-	
-	
-	
+				System.out.println("id = " + id + "; Nome = " + fn + "; Cognome= " + ln + "; Telefono = " + ph);
 
+			}
+
+			return lista;
+		} catch (Exception ex) {
+
+			ex.printStackTrace();
+			return new ArrayList<>();
+
+		} finally {
+
+			if (stat != null) {
+				try {
+					stat.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+	}
 }
