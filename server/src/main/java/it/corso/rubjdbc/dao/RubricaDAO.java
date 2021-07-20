2c0ea10512efedb75fb7b0ce53e9170384ee9720
giammarcoditomassi <giammarco.ditomassi@gmail.com>
2021-07-20 12:58:16 +0200

    aggiunta connessione, manca gestione eccezioni e controller

--- server/src/main/java/it/corso/rubjdbc/dao/RubricaDAO.java
+++ server/src/main/java/it/corso/rubjdbc/dao/RubricaDAO.java
@@ -1,5 +1,10 @@
 package it.corso.rubjdbc.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import it.corso.rubjdbc.model.Contatto;
@@ -25,7 +30,29 @@ public class RubricaDAO {
 	}
 	
 	public List<Contatto> trovaTutti(){
-		
+		Connection con= null;
+		Statement stmt = null;
+		List<Contatto> lista = new ArrayList <>();
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/rubricarand", "root", "");
+			
+			Statement stat = con.createStatement();
+			String sql = "select * from contatto";
+			
+			ResultSet res = stmt.executeQuery(sql);
+			
+			while (res.next()) {
+				long id = res.getLong("id");
+				String fn = res.getString("nome");
+				String ln = res.getString("cognome");
+				String ph = res.getString("telefono");
+				
+				Contatto cx = new Contatto(id, fn,ln, ph);
+				cx.setId(id);
+				lista.add(cx);
+				System.out.println("id = " + id + "; Nome = " + fn +"; Cognome= "+ ln + "; Telefono = " + ph);
+				
+			}
 		
 		
 		return null;
